c505d88c81a45bd72e64105baa2f913993433775
israrwali <157619032+israrwali@users.noreply.github.com>
2024-02-14 21:36:24 +0500

    Update classexample2.py

--- classexample2.py
+++ classexample2.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 st.title("Welcome Bano Qabil")
 
-creatingdataset
+#creating dataset
 
 df=pd.DataFrame({'first':[1,2,3,4,5,],'second':[6,7,8,9,10]})
 st.write(df)
 
-#or
+# or
 df  #magic comments
